1ec65fe03287663fafa0e35c5cdeeb95d7589760
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-03-25 16:50:26 -0500

    ENHANCEMENT(BASE): configured commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-full-stop': [2, 'never'],
+    'scope-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'upper-case'],
+    'scope-enum': [
+			2,
+			'always',
+      [
+        'BASE',
+      ],
+    ],
+    'type-case': [2, 'always', 'upper-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [
+			2,
+			'always',
+			[
+        'ENHANCEMENT',
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test'
+			]
+		]
+  }
+};
